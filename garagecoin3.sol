cca40221a2832fa5435d859c432e3c54026f4b13
burner202burner <167701543+burner202burner@users.noreply.github.com>
2025-01-22 10:43:24 -0500

    files222

---
+++ garagecoin3.sol
@@ -0,0 +1,444 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-04-09
+*/
+
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+
+/**
+ * @title IERC1363 Interface
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Interface for a Payable Token contract as defined in
+ *  https://eips.ethereum.org/EIPS/eip-1363
+ */
+interface IERC1363 is IERC20, IERC165 {
+    /**
+     * @notice Transfer tokens from `msg.sender` to another address and then call `onTransferReceived` on receiver
+     * @param recipient address The address which you want to transfer to
+     * @param amount uint256 The amount of tokens to be transferred
+     * @return true unless throwing
+     */
+    function transferAndCall(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @notice Transfer tokens from `msg.sender` to another address and then call `onTransferReceived` on receiver
+     * @param recipient address The address which you want to transfer to
+     * @param amount uint256 The amount of tokens to be transferred
+     * @param data bytes Additional data with no specified format, sent in call to `recipient`
+     * @return true unless throwing
+     */
+    function transferAndCall(
+        address recipient,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bool);
+
+    /**
+     * @notice Transfer tokens from one address to another and then call `onTransferReceived` on receiver
+     * @param sender address The address which you want to send tokens from
+     * @param recipient address The address which you want to transfer to
+     * @param amount uint256 The amount of tokens to be transferred
+     * @return true unless throwing
+     */
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @notice Transfer tokens from one address to another and then call `onTransferReceived` on receiver
+     * @param sender address The address which you want to send tokens from
+     * @param recipient address The address which you want to transfer to
+     * @param amount uint256 The amount of tokens to be transferred
+     * @param data bytes Additional data with no specified format, sent in call to `recipient`
+     * @return true unless throwing
+     */
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bool);
+
+    /**
+     * @notice Approve the passed address to spend the specified amount of tokens on behalf of msg.sender
+     * and then call `onApprovalReceived` on spender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender address The address which will spend the funds
+     * @param amount uint256 The amount of tokens to be spent
+     */
+    function approveAndCall(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @notice Approve the passed address to spend the specified amount of tokens on behalf of msg.sender
+     * and then call `onApprovalReceived` on spender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender address The address which will spend the funds
+     * @param amount uint256 The amount of tokens to be spent
+     * @param data bytes Additional data with no specified format, sent in call to `spender`
+     */
+    function approveAndCall(
+        address spender,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bool);
+}
+
+/**
+ * @title IERC1363Receiver Interface
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Interface for any contract that wants to support transferAndCall or transferFromAndCall
+ *  from ERC1363 token contracts as defined in
+ *  https://eips.ethereum.org/EIPS/eip-1363
+ */
+interface IERC1363Receiver {
+    /**
+     * @notice Handle the receipt of ERC1363 tokens
+     * @dev Any ERC1363 smart contract calls this function on the recipient
+     * after a `transfer` or a `transferFrom`. This function MAY throw to revert and reject the
+     * transfer. Return of other than the magic value MUST result in the
+     * transaction being reverted.
+     * Note: the token contract address is always the message sender.
+     * @param operator address The address which called `transferAndCall` or `transferFromAndCall` function
+     * @param sender address The address which are token transferred from
+     * @param amount uint256 The amount of tokens transferred
+     * @param data bytes Additional data with no specified format
+     * @return `bytes4(keccak256("onTransferReceived(address,address,uint256,bytes)"))` unless throwing
+     */
+    function onTransferReceived(
+        address operator,
+        address sender,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
+/**
+ * @title IERC1363Spender Interface
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Interface for any contract that wants to support approveAndCall
+ *  from ERC1363 token contracts as defined in
+ *  https://eips.ethereum.org/EIPS/eip-1363
+ */
+interface IERC1363Spender {
+    /**
+     * @notice Handle the approval of ERC1363 tokens
+     * @dev Any ERC1363 smart contract calls this function on the recipient
+     * after an `approve`. This function MAY throw to revert and reject the
+     * approval. Return of other than the magic value MUST result in the
+     * transaction being reverted.
+     * Note: the token contract address is always the message sender.
+     * @param sender address The address which called `approveAndCall` function
+     * @param amount uint256 The amount of tokens to be spent
+     * @param data bytes Additional data with no specified format
+     * @return `bytes4(keccak256("onApprovalReceived(address,uint256,bytes)"))` unless throwing
+     */
+    function onApprovalReceived(
+        address sender,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
+/**
+ * @title ERC1363
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Implementation of an ERC1363 interface
+ */
+abstract contract ERC1363 is ERC20, IERC1363, ERC165 {
+    using Address for address;
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return interfaceId == type(IERC1363).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev Transfer tokens to a specified address and then execute a callback on recipient.
+     * @param recipient The address to transfer to.
+     * @param amount The amount to be transferred.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function transferAndCall(address recipient, uint256 amount) public virtual override returns (bool) {
+        return transferAndCall(recipient, amount, "");
+    }
+
+    /**
+     * @dev Transfer tokens to a specified address and then execute a callback on recipient.
+     * @param recipient The address to transfer to
+     * @param amount The amount to be transferred
+     * @param data Additional data with no specified format
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function transferAndCall(
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        transfer(recipient, amount);
+        require(_checkAndCallTransfer(_msgSender(), recipient, amount, data), "ERC1363: _checkAndCallTransfer reverts");
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another and then execute a callback on recipient.
+     * @param sender The address which you want to send tokens from
+     * @param recipient The address which you want to transfer to
+     * @param amount The amount of tokens to be transferred
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        return transferFromAndCall(sender, recipient, amount, "");
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another and then execute a callback on recipient.
+     * @param sender The address which you want to send tokens from
+     * @param recipient The address which you want to transfer to
+     * @param amount The amount of tokens to be transferred
+     * @param data Additional data with no specified format
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        transferFrom(sender, recipient, amount);
+        require(_checkAndCallTransfer(sender, recipient, amount, data), "ERC1363: _checkAndCallTransfer reverts");
+        return true;
+    }
+
+    /**
+     * @dev Approve spender to transfer tokens and then execute a callback on recipient.
+     * @param spender The address allowed to transfer to
+     * @param amount The amount allowed to be transferred
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function approveAndCall(address spender, uint256 amount) public virtual override returns (bool) {
+        return approveAndCall(spender, amount, "");
+    }
+
+    /**
+     * @dev Approve spender to transfer tokens and then execute a callback on recipient.
+     * @param spender The address allowed to transfer to.
+     * @param amount The amount allowed to be transferred.
+     * @param data Additional data with no specified format.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function approveAndCall(
+        address spender,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        approve(spender, amount);
+        require(_checkAndCallApprove(spender, amount, data), "ERC1363: _checkAndCallApprove reverts");
+        return true;
+    }
+
+    /**
+     * @dev Internal function to invoke `onTransferReceived` on a target address
+     *  The call is not executed if the target address is not a contract
+     * @param sender address Representing the previous owner of the given token value
+     * @param recipient address Target address that will receive the tokens
+     * @param amount uint256 The amount mount of tokens to be transferred
+     * @param data bytes Optional data to send along with the call
+     * @return whether the call correctly returned the expected magic value
+     */
+    function _checkAndCallTransfer(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual returns (bool) {
+        if (!recipient.isContract()) {
+            return false;
+        }
+        bytes4 retval = IERC1363Receiver(recipient).onTransferReceived(_msgSender(), sender, amount, data);
+        return (retval == IERC1363Receiver(recipient).onTransferReceived.selector);
+    }
+
+    /**
+     * @dev Internal function to invoke `onApprovalReceived` on a target address
+     *  The call is not executed if the target address is not a contract
+     * @param spender address The address which will spend the funds
+     * @param amount uint256 The amount of tokens to be spent
+     * @param data bytes Optional data to send along with the call
+     * @return whether the call correctly returned the expected magic value
+     */
+    function _checkAndCallApprove(
+        address spender,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual returns (bool) {
+        if (!spender.isContract()) {
+            return false;
+        }
+        bytes4 retval = IERC1363Spender(spender).onApprovalReceived(_msgSender(), amount, data);
+        return (retval == IERC1363Spender(spender).onApprovalReceived.selector);
+    }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ * @title TokenRecover
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Allows owner to recover any ERC20 sent into the contract
+ */
+contract TokenRecover is Ownable {
+    /**
+     * @dev Remember that only owner can call so be careful when use on contracts generated from other contracts.
+     * @param tokenAddress The token contract address
+     * @param tokenAmount Number of tokens to be sent
+     */
+    function recoverERC20(address tokenAddress, uint256 tokenAmount) public virtual onlyOwner {
+        IERC20(tokenAddress).transfer(owner(), tokenAmount);
+    }
+}
+
+/**
+ * @title PowerfulERC20
+ * @author Veshi
+ * @dev Implementation of the PowerfulERC20
+ */
+contract PowerfulERC20 is
+    ERC20Decimals,
+    ERC20Capped,
+    ERC20Mintable,
+    ERC20Burnable,
+    ERC1363,
+    TokenRecover,
+    Roles,
+    ServicePayer
+{
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_,
+        uint256 cap_,
+        uint256 initialBalance_,
+        address payable feeReceiver_
+    )
+        payable
+        ERC20(name_, symbol_)
+        ERC20Decimals(decimals_)
+        ERC20Capped(cap_)
+        ServicePayer(feeReceiver_, "PowerfulERC20")
+    {
+        // Immutable variables cannot be read during contract creation time
+        // https://github.com/ethereum/solidity/issues/10463
+        require(initialBalance_ <= cap_, "ERC20Capped: Cap exceeded");
+        ERC20._mint(_msgSender(), initialBalance_);
+    }
+
+    function decimals() public view virtual override(ERC20, ERC20Decimals) returns (uint8) {
+        return super.decimals();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC1363) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev Function to mint tokens.
+     *
+     * NOTE: restricting access to addresses with MINTER role. See {ERC20Mintable-mint}.
+     *
+     * @param account The address that will receive the minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) onlyMinter {
+        super._mint(account, amount);
+    }
+
+    /**
+     * @dev Function to stop minting new tokens.
+     *
+     * NOTE: restricting access to owner only. See {ERC20Mintable-finishMinting}.
+     */
+    function _finishMinting() internal override onlyOwner {
+        super._finishMinting();
+    }
+}
